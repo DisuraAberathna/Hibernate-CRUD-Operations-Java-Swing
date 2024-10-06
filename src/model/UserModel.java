e326130d924562da63a21a336fb80cebafd3463a
Disura Chathwara <disura2005@gmail.com>
2024-10-06 16:22:07 +0530

    add, update, delete users methods

--- src/model/UserModel.java
+++ src/model/UserModel.java
@@ -19,8 +19,31 @@ public class UserModel {
     static Session session = HibernateUtil.getSessionFactory().openSession();
 
     public static List<User> searchUsers() {
-            Criteria userCriteria = session.createCriteria(User.class);
-            userCriteria.addOrder(Order.asc("id"));
-            return userCriteria.list();
+        Criteria userCriteria = session.createCriteria(User.class);
+        userCriteria.addOrder(Order.asc("id"));
+        return userCriteria.list();
+    }
+
+    public static void saveUser(User user) {
+        session.save(user);
+        session.beginTransaction().commit();
+    }
+
+    public static void updateUser(User user, int id) {
+        User u = (User) session.get(User.class, id);
+        u.setF_name(user.getF_name());
+        u.setL_name(user.getL_name());
+        u.setUsername(user.getUsername());
+        u.setDob(user.getDob());
+
+        session.update(u);
+        session.beginTransaction().commit();
+    }
+
+    public static void deleteUser(int id) {
+        User user = (User) session.get(User.class, id);
+
+        session.delete(user);
+        session.beginTransaction().commit();
     }
 }
